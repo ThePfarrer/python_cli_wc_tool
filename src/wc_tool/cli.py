b681c81c4ae392b5a9c452e6a7015e47b2476c01
Uche Okereke <orezpablo@gmail.com>
2024-11-13 13:57:32 +0100

    Step Three to Six: Refactored whole program

--- src/wc_tool/cli.py
+++ src/wc_tool/cli.py
@@ -1,29 +1,81 @@
 import click
 
-from .wc import count_bytes, count_lines
+from wc_tool import wc
 
 
 @click.command()
-@click.option("-c", "--bytees", is_flag=True, help="print the byte counts")
+@click.option("-c", "--bytes", "bytes_flag", is_flag=True, help="print the byte counts")
 @click.option("-l", "--lines", is_flag=True, help="print the newline counts")
 @click.option("-w", "--words", is_flag=True, help="print the word count")
 @click.option("-m", "--chars", is_flag=True, help="print the character counts")
-@click.argument("file_path")
-def main(bytees, lines, words, chars, file_path):
+@click.argument("file_paths", required=False, nargs=-1)
+def main(bytes_flag, lines, words, chars, file_paths):
     """
     Print newline, word, and byte counts for each FILE, and a total line if more than one FILE is specified.  A word is a non-zero-length sequence of characters delimited by white space.
 
     With no FILE, or when FILE is -, read standard input.
     """
-    if bytees:
-        number_of_bytes = count_bytes(file_path)
-        click.echo(number_of_bytes)
-    elif lines:
-        number_of_lines = count_lines(file_path)
-        click.echo(number_of_lines)
-    pass
+    # Initialize totals
+
+    (
+        total_number_of_bytes,
+        total_number_of_lines,
+        total_number_of_words,
+        total_number_of_chars,
+    ) = (0, 0, 0, 0)
+
+    if not file_paths or (file_paths == ("-",)):
+        content = wc.read_file()
+
+        click.echo(f"{output(content, lines, words, bytes_flag, chars)}")
+    else:
+        for file in file_paths:
+            content = wc.read_file(file)
+
+            if isinstance(content, Exception):
+                number_of_lines, number_of_words, number_of_bytes, number_of_chars = (
+                    0,
+                    0,
+                    0,
+                    0,
+                )
+                click.echo(content)
+            else:
+                (
+                    number_of_lines,
+                    number_of_words,
+                    number_of_bytes,
+                    number_of_chars,
+                ) = content
+                click.echo(f"{output(content, lines, words, bytes_flag, chars)} {file}")
+
+            total_number_of_lines += number_of_lines
+            total_number_of_words += number_of_words
+            total_number_of_bytes += number_of_bytes
+            total_number_of_chars += number_of_chars
+
+    if len(file_paths) > 1:
+        totals = (
+            total_number_of_lines,
+            total_number_of_words,
+            total_number_of_bytes,
+            total_number_of_chars,
+        )
+        click.echo(f"{output(totals, lines, words, bytes_flag, chars)} total")
 
 
-# def process_input()
+def output(content_count, lines, words, bytes_flag, chars):
+    # Handle default flags when none are specified
+    if not any([lines, words, bytes_flag, chars]):
+        lines, words, bytes_flag = True, True, True
+
+    line_str = f"{content_count[0]:>6} " * lines
+    word_str = f"{content_count[1]:>6} " * words
+    byte_str = f"{content_count[2]:>6} " * bytes_flag
+    char_str = f"{content_count[3]:>6} " * chars
+
+    return f"{line_str}{word_str}{byte_str}{char_str}"
+
+
 if __name__ == "__main__":
     main()
--- src/wc_tool/wc.py
+++ src/wc_tool/wc.py
@@ -1,42 +1,30 @@
+import fileinput
+import locale
 import sys
 
 
-def read_file(file_path=None, flag="r"):
+def read_file(file_path=None):
+    """
+    Reads a file or standard input and calculates line, word, byte, and character counts.
+    :param file_path: The path of the file to read. If None, reads from stdin.
+    :return: A tuple with counts (line_count, word_count, byte_count, char_count) or an error.
+    """
+    # Handle reading from stdin with optional parameters
+    sys.argv[1:] = sys.argv[1:] if file_path else []
+
+    word_count = byte_count = char_count = line_count = 0
+    encoding = locale.getpreferredencoding().lower()
+
     try:
-        chunk_size = 1024
-        if file_path:
-            with open(file_path, flag) as file:
-                while chunk := file.read(chunk_size):
-                    yield chunk
-        else:
-            while chunk := sys.stdin.read(chunk_size):
-                yield chunk
+        with fileinput.input(files=file_path, mode="rb") as file:
+            for line_count, line in enumerate(file, 1):
+                word_count += len(line.split())
+                byte_count += len(line)
+                char_count += len(line.decode(encoding))
+
+            return line_count, word_count, byte_count, char_count
 
     except FileNotFoundError:
-        yield FileNotFoundError(f"pywc: {file_path}: No such file or directory")
+        return FileNotFoundError(f"pywc: {file_path}: No such file or directory")
     except IOError:
-        yield IOError(f"pywc: {file_path}: There was an issue reading the file")
-
-
-def count_bytes(file_path):
-    count = 0
-    file_content = read_file(file_path, "rb")
-    for content in file_content:
-        if isinstance(content, Exception):
-            return content
-        else:
-            count += len(content)
-
-    return f"{count} {file_path}"
-
-
-def count_lines(file_path):
-    count = 0
-    file_content = read_file(file_path, "r")
-    for content in file_content:
-        if isinstance(content, Exception):
-            return content
-        else:
-            count += content.count("\n")
-
-    return f"{count} {file_path}"
+        return IOError(f"pywc: {file_path}: There was an issue reading the file")
